17f2fba10e8d9a94bf5ceaa94ff4abeb30d81c43
IlyasIsLearning <151291039+7310810497109@users.noreply.github.com>
2025-12-07 22:33:04 +0700

    modified

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,23 +1,4 @@
 <?php
-<<<<<<< HEAD
-return [
-    
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    // Method harus '*' (artinya semua method dibolehkan)
-    'allowed_methods' => ['*'], 
-
-    // Origin harus spesifik URL React agar supports_credentials bekerja
-    'allowed_origins' => ['http://localhost:5173'], 
-
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
-    'supports_credentials' => true,
-
-];
-=======
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'auth/google/callback'],
@@ -36,4 +17,3 @@ return [
 
     'supports_credentials' => true,  
 ];
->>>>>>> 1c1f00469ff9d80460dcda467a83a5167f203fc4
